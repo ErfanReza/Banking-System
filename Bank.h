67bcffafde6631bb0bb887c2734c965b046d8da5
ErfanReza <erfanreza14@gmail.com>
2021-07-30 16:15:54 -0400

    First Commit

---
+++ Bank.h
@@ -0,0 +1,18 @@
+#include "Account.h"
+#include<map>
+
+
+class Bank
+{
+private:
+	map<long, Account> accounts;
+public:
+	Bank();
+	Account OpenAccount(string fname, string lname, float balance);
+	Account BalanceEnquiry(long accountNumber);
+	Account Deposit(long accountNumber, float amount);
+	Account Withdraw(long accountNumber, float amount);
+	void CloseAccount(long accountNumber);
+	void ShowAllAccounts();
+	~Bank();
+};
